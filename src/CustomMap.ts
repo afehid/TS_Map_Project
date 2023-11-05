023f713c17a0e1f9cbfa880c4178f77e184266c4
afahid <a.fehid@elarabygroup.com>
2023-11-05 16:03:43 +0200

    Adding Project of google Maps

---
+++ src/CustomMap.ts
@@ -0,0 +1,42 @@
+//Instructions to every other class
+//on how they can be an argument to 'addMarker'
+
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googlemap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googlemap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googlemap,
+      position: { lat: mappable.location.lat, lng: mappable.location.lng },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googlemap, marker);
+    });
+  }
+}
